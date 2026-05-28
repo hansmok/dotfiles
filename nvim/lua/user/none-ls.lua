df6797808e2a9ce45ae07e1237501a0293b57502
Hans Mok <mok_hans@yahoo.com>
2026-05-27 17:13:58 -0700

    Add nvim dotfiles

---
+++ nvim/lua/user/none-ls.lua
@@ -0,0 +1,29 @@
+local M = {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
+
+function M.config()
+  local null_ls = require "null-ls"
+
+  local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
+
+  null_ls.setup {
+    debug = false,
+    sources = {
+      -- Lua Formatting
+      formatting.stylua,
+
+      -- Web/Frontend Formatting
+      formatting.prettier,
+
+      -- Miscellaneous Builtins
+      null_ls.builtins.completion.spell,
+    },
+  }
+end
+
+return M
